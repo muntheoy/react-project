574618932b92e9d8d5c4044ec2e1abc391c85845
Elizaveta Savchenko <savchenko.liza.666@gmail.com>
2023-12-12 20:42:25 +0600

    Create top navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import './App.css'; // Импортируйте файл со стилями
+import './styles/App.css';   
+import TopNavigation from './components/TopNavigation';
+
 
 function App() {
-  const textStyle: React.CSSProperties = {
-    fontFamily: 'MulishRegular, sans-serif', // Используйте название шрифта, заданное в @font-face
-    fontSize: '48px', // Ваш размер шрифта
-     // Указывается в @font-face, но может быть переопределено здесь
-    
-  };
+  
 
   return (
     <div className="App">
-      <p style={textStyle}>Our service provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+      <TopNavigation/>
     </div>
   );
 }
