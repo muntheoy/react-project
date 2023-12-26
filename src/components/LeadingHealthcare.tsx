664751bb7be5f39b37d6d2b4f8dd399b1f8babf4
Elizaveta Savchenko <savchenko.liza.666@gmail.com>
2023-12-26 22:27:33 +0600

    Fix leading healthcare

--- src/components/LeadingHealthcare.tsx
+++ src/components/LeadingHealthcare.tsx
@@ -3,17 +3,20 @@ import "../styles/LeadingHealthcare.css";
 
 
 const LeadingHealthcare: React.FC = () =>{
-
+    const img = require("../assets/images/leading-healthcare-element-1.png");
+    const title = "Leading healthcare providers"
+    const subtitle = "We provides progressive, and affordable healthcare, accessible on mobile and online for everyone. To us, it’s not just work. We take pride in the solutions we deliver";
+    const buttonText = "Learn more"
     return(
         <div className="LeadingHealthcare">
-            <img className="LeadingHealthcare-img" src={require("../assets/images/leading-healthcare-element-1.png")}/>
+            <img className="LeadingHealthcare-img" src={img}/>
             <div className="LeadingHealthcare-right-side">
                 <div className="LeadingHealthcare-text">
-                    <span className="LeadingHealthcare-text-title">Leading healthcare providers</span>
+                    <span className="LeadingHealthcare-text-title">{title}</span>
                     <div className="LeadingHealthcare-text-line"></div>
-                    <span className="LeadingHealthcare-text-subtitle">We provides progressive, and affordable healthcare, accessible on mobile and online for everyone. To us, it’s not just work. We take pride in the solutions we deliver</span>
+                    <span className="LeadingHealthcare-text-subtitle">{subtitle}</span>
                 </div>
-                <button className="LeadingHealthcare-button">Learn more</button>
+                <button className="LeadingHealthcare-button">{buttonText}</button>
             </div>
             
         </div>
