853fb1004cb96be111e8169d3382232d160a8478
Elizaveta Savchenko <savchenko.liza.666@gmail.com>
2023-12-26 22:32:52 +0600

    Fix mobile app block

--- src/components/MobileApp.tsx
+++ src/components/MobileApp.tsx
@@ -3,22 +3,25 @@ import "../styles/MobileApp.css";
 
 
 const MobileApp: React.FC = () =>{
-
+    const title = "Download our mobile apps";
+    const subtitle = "Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely";
+    const imgArrow = require("../assets/images/app-arrow-img.png");
+    const img = require("../assets/images/mobile-app-img.png");
     return(
         <div className="MobileApp">
             
             <div className="MobileApp-right-side">
                 <div className="MobileApp-text">
-                    <span className="MobileApp-text-title">Download our mobile apps</span>
+                    <span className="MobileApp-text-title">{title}</span>
                     <div className="MobileApp-text-line"></div>
-                    <span className="MobileApp-text-subtitle">Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely</span>
+                    <span className="MobileApp-text-subtitle">{subtitle}</span>
                 </div>
                 <button className="MobileApp-button">
                     <span>Download</span>
-                    <img src={require("../assets/images/app-arrow-img.png")} style={{width:'8%'}}/>
+                    <img className="MobileApp-img-arrow" src={imgArrow}/>
                     </button>
             </div>
-            <img className="MobileApp-img" src={require("../assets/images/mobile-app-img.png")}/>
+            <img className="MobileApp-img" src={img}/>
         </div>
     )
 }
